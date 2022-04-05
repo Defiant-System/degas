f2217b6f8f610ee78ff1f8a2526aab85736a2ec0
Hakan Bilgin <hbi99@hotmail.com>
2022-04-05 13:42:17 +0200

    color picker UI + UX

--- src/js/modules/colorpicker.js
+++ src/js/modules/colorpicker.js
@@ -8,10 +8,25 @@
 	dispatch(event) {
 		let APP = degas,
 			Self = APP.colorpicker,
+			name,
+			value,
 			el;
 		// console.log(event);
 		switch(event.type) {
-			case "test-event":
+			case "group-head":
+				el = $(event.target);
+				value = el.index();
+				el.parent().find(".active").removeClass("active");
+				el.addClass("active");
+
+				el = el.parent().nextAll(".group-body:first");
+				el.find("> div.active").removeClass("active");
+				el.find(`> div:nth(${value})`).addClass("active");
+				break;
+			case "select-palette-color":
+				el = $(event.target);
+				el.parent().find(".active").removeClass("active");
+				el.addClass("active");
 				break;
 		}
 	}
