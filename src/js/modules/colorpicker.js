45cf030fa13d771995b3a5e06c25db6d955cba78
Hakan Bilgin <hbi99@hotmail.com>
2022-04-24 22:17:15 +0200

    color picker clean

--- src/js/modules/colorpicker.js
+++ src/js/modules/colorpicker.js
@@ -32,7 +32,7 @@
 			},
 		};
 		// "settings"
-		this.mode = "palette";  // palette RGBA HSVA
+		this.mode = "RGBA";  // palette RGBA HSVA
 		this.radius = 74;
 		this.origin = {};
 		// click on the right "tab"
@@ -45,7 +45,7 @@
 			Self = APP.colorpicker,
 			opacity,
 			rgb, hsv,
-			rad, sat,
+			rad, sat, tau,
 			top, left, height,
 			name, value,
 			pEl,
@@ -71,6 +71,7 @@
 				Self.origin.rgb = Color.hexToRgb(Self.origin.hex);
 				Self.origin.hsv = Color.hexToHsv(Self.origin.hex);
 				Self.dispatch({ type: `set-fields-${Self.mode}`, ...Self.origin });
+				Self.dispatch({ type: "set-wheel-range", ...Self.origin });
 
 				value = window.getBoundingClientRect(event.el[0]);
 				top = value.top - (+Self.els.el.prop("offsetHeight") >> 1) + 12;
@@ -79,7 +80,8 @@
 
 				let doc = $(document),
 					func = event => {
-						if ($(event.target).parents(".color-picker").length) return;
+						let el = $(event.target);
+						if (el.hasClass("color-picker") || el.parents(".color-picker").length) return;
 						Self.els.el
 							.cssSequence("hide", "animationend", el =>
 								el.css({ top: -1e3, left: -1e3 }).removeClass("show hide"));
@@ -99,13 +101,16 @@
 				value = el.index();
 				el.parent().find(".active").removeClass("active");
 				el.addClass("active");
+				// set mode
+				Self.mode = el.data("id");
 				// show right "body"
 				el = el.parent().nextAll(".group-body:first");
 				el.find("> div.active").removeClass("active");
 				el.find(`> div:nth(${value})`).addClass("active");
-				// set mode
-				Self.mode = el.data("id");
-				Self.dispatch({ type: `set-fields-${Self.mode}`, ...Self.origin });
+				if (Self.origin.hex) {
+					// update dialog UI
+					Self.dispatch({ type: `set-fields-${Self.mode}`, ...Self.origin });
+				}
 				break;
 			case "set-fields-HSVA":
 				hsv = event.hsv || Self.origin.hsv;
@@ -154,11 +159,60 @@
 				top = Math.round(Math.sin(rad) * sat + Self.radius);
 				left = Math.round(Math.cos(rad) * sat + Self.radius);
 				Self.els.wCursor.css({ top, left });
-				// saturation cursor
+				// wheel darkness
 				opacity = hsv.v / 100;
+				Self.els.wheel.css({ opacity });
+				// saturation cursor
 				top = Math.round(height * (1-opacity));
 				Self.els.rCursor.css({ top });
-				Self.els.wheel.css({ opacity });
+				break;
+			case "set-rgba-R":
+			case "set-rgba-G":
+			case "set-rgba-B":
+				Self.origin.rgb = {
+					r: Math.round(+Self.els.groupRGBA.R.data("value") * 255),
+					g: Math.round(+Self.els.groupRGBA.G.data("value") * 255),
+					b: Math.round(+Self.els.groupRGBA.B.data("value") * 255),
+				};
+				Self.origin.hsv = Color.rgbToHsv(Self.origin.rgb);
+				Self.origin.hex = Color.rgbToHex(Self.origin.rgb);
+				Self.dispatch({ type: "set-wheel-range" });
+				Self.dispatch({ type: "update-origin-el" });
+				break;
+			case "set-rgba-A":
+				Self.origin.opacity = +Self.els.groupRGBA.A.data("value");
+				Self.dispatch({ type: "update-origin-el" });
+				break;
+			case "set-hsva-H":
+			case "set-hsva-S":
+			case "set-hsva-V":
+				Self.origin.hsv = {
+					h: Math.round(+Self.els.groupHSVA.H.data("value") * 360),
+					s: Math.round(+Self.els.groupHSVA.S.data("value") * 100),
+					v: Math.round(+Self.els.groupHSVA.V.data("value") * 100),
+				};
+				Self.origin.rgb = Color.hsvToRgb(Self.origin.hsv);
+				Self.origin.hex = Color.rgbToHex(Self.origin.rgb);
+				Self.dispatch({ type: "set-wheel-range" });
+				Self.dispatch({ type: "update-origin-el" });
+				break;
+			case "set-hsva-A":
+				Self.origin.opacity = +Self.els.groupHSVA.A.data("value");
+				Self.dispatch({ type: "update-origin-el" });
+				break;
+			case "update-RGBA":
+			case "update-HSVA":
+				tau = Math.PI * 2;
+				Self.origin.hsv = {
+					h: Math.round(Self.mod(Math.atan2(-event.y, -event.x) * (360 / tau), 360)),
+					s: Math.round(Math.min(Self.radius, Self.distance(event.left, event.top)) / Self.radius * 100),
+					v: Math.round(event.value * 100),
+				};
+				Self.origin.rgb = Color.hsvToRgb(Self.origin.hsv);
+				Self.origin.hex = Color.rgbToHex(Self.origin.rgb);
+				name = event.type.split("-")[1];
+				Self.dispatch({ type: "set-fields-"+ name });
+				Self.dispatch({ type: "update-origin-el" });
 				break;
 		}
 	},
@@ -169,12 +223,195 @@
 		return Math.sqrt(Math.pow(left - this.radius, 2) + Math.pow(top - this.radius, 2));
 	},
 	doField(event) {
-		
+		let Self = degas.colorpicker,
+			Drag = Self.drag;
+		switch(event.type) {
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+				// info about DnD event
+				let doc = $(document),
+					el = $(event.target);
+				// data for move event
+				Self.drag = {
+					el,
+					changeType: el.data("change"),
+					offset: +el.css("--value"),
+					clickX: event.clientX,
+					_max: Math.max,
+					_min: Math.min,
+					_round: Math.round,
+					doc,
+				};
+				// cover APP UI
+				APP.els.content.addClass("cover hideMouse");
+				// bind event handlers
+				Self.drag.doc.on("mousemove mouseup", Self.doField);
+				break;
+			case "mousemove":
+				let step = 0.003,
+					sVal = Drag.offset - ((Drag.clickX - event.clientX) * step),
+					sValue = Drag._min(Drag._max(step * Drag._round(sVal / step), 0), 1),
+					value = sValue.toFixed(3);
+				Drag.el
+					.data({ value })
+					.css({ "--value": value });
+				// forward value to dispatch
+				Self.dispatch({ type: Drag.changeType, value });
+				break;
+			case "mouseup":
+				// uncover APP UI
+				APP.els.content.removeClass("cover hideMouse");
+				// unbind event handlers
+				Self.drag.doc.off("mousemove mouseup", Self.doField);
+				break;
+		}
 	},
 	doWrapper(event) {
-		
+		let Self = degas.colorpicker,
+			Drag = Self.drag;
+		switch(event.type) {
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+				// collect event info
+				let doc = $(document),
+					el = $(event.target).find(".cursor"),
+					group = Self.els.groupHSVA,
+					clientY = event.clientY,
+					clientX = event.clientX,
+					dim = {
+						top: parseInt(Self.els.rCursor.css("top"), 10),
+						height: parseInt(Self.els.range.css("height"), 10),
+					},
+					offset = {
+						left: event.offsetX,
+						top: event.offsetY,
+					},
+					click = {
+						x: event.clientX,
+						y: event.clientY,
+					};
+				if (!event.isFake) {
+					// remove "active" indicator from palette
+					Self.els.palette.find(".active").removeClass("active");
+				}
+				// move cursor
+				el.css(offset);
+				// create drag
+				Self.drag = {
+					el,
+					click,
+					offset,
+					group,
+					value: (1-(dim.top / dim.height)),
+					limit: (left, top) => {
+						var dist = Self.distance(left, top),
+							rad;
+						if (dist <= Self.radius) return { left, top };
+						else {
+							left = left - Self.radius;
+							top = top - Self.radius;
+							rad = Math.atan2(top, left);
+							return {
+								left: Math.round(Math.cos(rad) * Self.radius + Self.radius),
+								top: Math.round(Math.sin(rad) * Self.radius + Self.radius),
+							}
+						}
+					},
+					doc,
+				};
+				// trigger update
+				Self.doWrapper({ type: "mousemove", clientY, clientX });
+				if (!event.isFake) {
+					// cover layout
+					Self.els.content.addClass("cover hideMouse");
+					// bind event
+					Self.drag.doc.on("mousemove mouseup", Self.doWrapper);
+				}
+				break;
+			case "mousemove":
+				let top = event.clientY + Drag.offset.top - Drag.click.y,
+					left = event.clientX + Drag.offset.left - Drag.click.x,
+					limited = Drag.limit(left, top),
+					x = Self.radius - limited.left,
+					y = Self.radius - limited.top;
+				// cursor position
+				Drag.el.css(limited);
+				// update fields
+				Self.dispatch({ type: `update-${Self.mode}`, ...limited, x, y, value: Drag.value });
+				break;
+			case "mouseup":
+				// uncover layout
+				Self.els.content.removeClass("cover hideMouse");
+				// unbind event
+				Self.drag.doc.off("mousemove mouseup", Self.doWrapper);
+				// reset drag object
+				delete Self.drag;
+				break;
+		}
 	},
-	doRange(event) {
-		
+	doRange(event) {let Self = degas.colorpicker,
+			Drag = Self.drag;
+		switch(event.type) {
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+				// cover layout
+				Self.els.content.addClass("cover hideMouse");
+				// collect event info
+				let doc = $(document),
+					el = $(event.target).find(".cursor"),
+					group = Self.els.groupHSVA,
+					target = Self.els.wheel,
+					offset = { top: event.offsetY - 3 },
+					clientY = event.clientY,
+					click = { y: clientY },
+					constrain = {
+						minY: 0,
+						maxY: +Self.els.range.prop("offsetHeight"),
+					},
+					wheel = {
+						top: parseInt(Self.els.wCursor.css("top"), 10),
+						left: parseInt(Self.els.wCursor.css("left"), 10),
+					},
+					_max = Math.max,
+					_min = Math.min,
+					mode = Self.mode;
+				if (event.clientX !== 0 && clientY !== 0) {
+					// remove "active" indicator from palette
+					Self.els.palette.find(".active").removeClass("active");
+				}
+				// more calc
+				wheel.x = Self.radius - wheel.left;
+				wheel.y = Self.radius - wheel.top;
+				// move cursor
+				el.css(offset);
+				// create drag
+				Self.drag = { el, target, group, click, offset, constrain, wheel, _min, _max, doc };
+				// trigger update
+				Self.doRange({ type: "mousemove", clientY });
+				// bind event
+				Self.drag.doc.on("mousemove mouseup", Self.doRange);
+				break;
+			case "mousemove":
+				let top = Drag._min(Drag._max(event.clientY + Drag.offset.top - Drag.click.y, Drag.constrain.minY), Drag.constrain.maxY),
+					value = 1 - (top / Drag.constrain.maxY);
+				// cursor position
+				Drag.el.css({ top });
+				// wheel opacity
+				Drag.target.css({ opacity: value });
+				// update fields
+				Self.dispatch({ type: `update-${Self.mode}`, value, ...Drag.wheel });
+				break;
+			case "mouseup":
+				// uncover layout
+				Self.els.content.removeClass("cover hideMouse");
+				// unbind event
+				Self.drag.doc.off("mousemove mouseup", Self.doRange);
+				// reset drag object
+				delete Self.drag;
+				break;
+		}
 	}
 }
